ecaeeedebd0db676685ae5b1dd91ed2b829648c3
Xavier <axc.llumiquinga@yavirac.edu.ec>
2024-05-29 10:12:52 -0400

    Frontend conectado a al backend falta comprobar y agregar lo trabajado en clase solo tiene la estructura basica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BackendService } from './Services/backend.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontendClases';
+export class AppComponent implements OnInit {
+  message: string | undefined;
+
+  constructor(private backendService: BackendService) {}
+
+  ngOnInit() {
+    this.backendService.getHello().subscribe(
+      (response) => {
+        this.message = response;
+        console.log('Message from backend:', this.message);
+      },
+      (error) => {
+        console.error('Error fetching message:', error);
+      }
+    );
+  }
 }
